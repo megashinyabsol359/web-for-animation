d265cde0f8d864f95caf3fe16a897e16c897ab45
Nidaime-chan <104910191+megashinyabsol359@users.noreply.github.com>
2025-08-29 21:17:10 +0700

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function openPoster(el) {
-  const src = el.querySelector('img').src;
-  const lb = document.getElementById('lightbox');
-  const img = document.getElementById('lb');
-  img.src = src;
-  if (lb.showModal) lb.showModal();
-  else lb.setAttribute('open', '');
-  lb.addEventListener('click', () => lb.close());
-}
